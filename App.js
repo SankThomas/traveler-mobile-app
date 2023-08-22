54d06d4fbcdb2dd89ad56fd578faff6a47797398
sankthomas <thomassankara74@gmail.com>
2023-08-23 00:57:07 +0300

    set up routing

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
-export default function App() {
+// screens
+import Homepage from "./screens/Homepage";
+import Landing from "./screens/Landing";
+import Destination from "./screens/Destination";
+import Settings from "./screens/Settings";
+
+const BottomTabs = createMaterialBottomTabNavigator();
+
+function Tabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="Homepage" component={Homepage} />
+      <BottomTabs.Screen name="Settings" component={Settings} />
+    </BottomTabs.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="transparent" />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+          animation: "slide_from_right",
+        }}
+      >
+        <Stack.Screen
+          name="Tabs"
+          component={Tabs}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="Homepage" component={Homepage} />
+        <Stack.Screen name="Destination" component={Destination} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
