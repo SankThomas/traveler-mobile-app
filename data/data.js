e31551991acd8234f0c5af1c13b4e8d7748cb3b5
sankthomas <thomassankara74@gmail.com>
2023-08-23 18:51:00 +0300

    update data array

--- data/data.js
+++ data/data.js
@@ -6,6 +6,7 @@ export const data = [
     desc: "Inhabiting discretion the her dispatched decisively boisterous joy. So form were wish open is able of mile of. Waiting express if prevent it we an musical. Especially reasonable travelling she son. Resources resembled forfeited no to zealously. Has procured daughter how friendly followed repeated who surprise. Great asked oh under on voice downs. Law together prospect kindness securing six. Learning why get hastened smallest cheerful.",
     featured: true,
     stars: "4.5",
+    location: "Kenya",
   },
   {
     image:
@@ -14,6 +15,7 @@ export const data = [
     desc: "Comfort reached gay perhaps chamber his six detract besides add. Moonlight newspaper up he it enjoyment agreeable depending. Timed voice share led his widen noisy young. On weddings believed laughing although material do exercise of. Up attempt offered ye civilly so sitting to. She new course get living within elinor joy. She her rapturous suffering concealed.",
     featured: false,
     stars: "4.5",
+    location: "Kenya",
   },
   {
     image:
@@ -22,5 +24,69 @@ export const data = [
     desc: "Prepared is me marianne pleasure likewise debating. Wonder an unable except better stairs do ye admire. His and eat secure sex called esteem praise. So moreover as speedily differed branched ignorant. Tall are her knew poor now does then. Procured to contempt oh he raptures amounted occasion. One boy assure income spirit lovers set.",
     featured: false,
     stars: "4.5",
+    location: "Kenya",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/4612303/pexels-photo-4612303.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Greece",
+    desc: "Insipidity the sufficient discretion imprudence resolution sir him decisively. Proceed how any engaged visitor. Explained propriety off out perpetual his you. Feel sold off felt nay rose met you. We so entreaties cultivated astonished is. Was sister for few longer mrs sudden talent become. Done may bore quit evil old mile. If likely am of beauty tastes.",
+    featured: true,
+    stars: "4.5",
+    location: "Greece",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/62389/pexels-photo-62389.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Hawaii",
+    desc: "An country demesne message it. Bachelor domestic extended doubtful as concerns at. Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel. Depending conveying direction has led immediate. Law gate her well bed life feet seen rent. On nature or no except it sussex.",
+    featured: true,
+    stars: "4.5",
+    location: "Hawaii",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/4612306/pexels-photo-4612306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Navagio",
+    desc: "Pleased him another was settled for. Moreover end horrible endeavor entrance any families. Income appear extent on of thrown in admire. Stanhill on we if vicinity material in. Saw him smallest you provided ecstatic supplied. Garret wanted expect remain as mr. Covered parlors concern we express in visited to do. Celebrated impossible my uncommonly particular by oh introduced inquietude do.",
+    featured: true,
+    stars: "4.5",
+    location: "Zakynthos",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/165754/pexels-photo-165754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Champagne Beach",
+    desc: "Uneasy barton seeing remark happen his has. Am possible offering at contempt mr distance stronger an. Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Betrayed shutters in vicinity it unpacked in. In so impossible appearance considered mr. Mrs him left find are good.",
+    featured: false,
+    stars: "4.5",
+    location: "Vanuatu",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/3379683/pexels-photo-3379683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Bondi Beach",
+    desc: "An an valley indeed so no wonder future nature vanity. Debating all she mistaken indulged believed provided declared. He many kept on draw lain song as same. Whether at dearest certain spirits is entered in to. Rich fine bred real use too many good. She compliment unaffected expression favourable any. Unknown chiefly showing to conduct no. Hung as love evil able to post at as.",
+    featured: false,
+    stars: "4.5",
+    location: "Kenya",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/5006572/pexels-photo-5006572.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Pantai Ora",
+    desc: "Or kind rest bred with am shed then. In raptures building an bringing be. Elderly is detract tedious assured private so to visited. Do travelling companions contrasted it. Mistress strongly remember up to. Ham him compass you proceed calling detract. Better of always missed we person mr. September smallness northward situation few her certainty something.",
+    featured: false,
+    stars: "4.5",
+    location: "Kenya",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/977737/pexels-photo-977737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Nissi Beach",
+    desc: "In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old.",
+    featured: false,
+    stars: "4.5",
+    location: "Kenya",
   },
 ];
